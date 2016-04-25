e8856f6c620c14e941391b5e1b30f85088c2c2cf
Sean <SeanLynn@users.noreply.github.com>
2016-04-25 23:19:40 +0800

    Update and rename Heap_Sort.cpp to Heap_Sort_Priority_Queue.cpp

--- src/Sort/Heap_Sort_Priority_Queue.cpp
+++ src/Sort/Heap_Sort_Priority_Queue.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <limits.h>
 using namespace std;
 
 class Heap
@@ -12,10 +14,57 @@ public:
     Heap(int length){
         // array[0] is empty
         array.resize(length+1);
-        heap_size = 1;
+        heap_size = 0;
     }
     ~Heap(){}
     
+    int heap_maximum(){
+        // the max element
+        return array[1];
+    }
+
+    void max_heap_insert(int key){
+        if(add(INT_MIN)){
+            heap_increase_key(heap_size, key);
+        }
+    }
+
+    int heap_extract_max(){
+        // remove the max element, return it, and adjust the structure
+        if(heap_size < 1){
+            cout << "heap underflow" << endl;
+            return 0;
+        }
+        int max = array[1];
+        array[1] = array[heap_size];
+        --heap_size;
+        max_heapify_loop(1);
+        return max;
+    }
+
+    void heap_increase_key(int i, int key){
+        if(key < array[i]){
+            cout << "new key is smaller than current key" << endl;
+            return ;
+        }
+        array[i] = key;
+        while(i > 1 && array[parent(i)] < array[i]){
+            swap(array[parent(i)], array[i]);
+            i = parent(i);
+        }
+    }
+
+    void heap_sort(){
+        build_max_heap();
+        int size = heap_size;
+        for(int i = heap_size; i > 1; --i){
+            swap(array[i], array[1]);
+            --heap_size;
+            max_heapify_loop(1);
+        }
+        heap_size = size;
+    }
+
     void max_heapify_recursive(int i){
         int l = left(i);
         int r = right(i);
@@ -90,22 +139,24 @@ public:
     void build_max_heap(){
         for(int i = heap_size/2; i > 0; --i){
             max_heapify_loop(i);
-
         }
     }
 
     //for initialize the vector
-    void add(int data){
+    bool add(int data){
         if(heap_size<length){
-            array[heap_size] = data;
             ++heap_size;
+            array[heap_size] = data;
+            return true;
         }else{
+            //the exception can be handled by resize the length of the heap
             cout << "heap is full!" << endl;
+            return false;
         }
     }
 
     void print(){
-        for(int i = 1; i < heap_size; ++i){
+        for(int i = 1; i <= heap_size; ++i){
             cout << array[i] << ' ';
         }
         cout << endl;
@@ -129,6 +180,7 @@ public:
 int main(int argc, char const *argv[])
 {
     Heap h(10);
+    //1,
     // h.add(16);
     // h.add(4);
     // h.add(10);
@@ -139,22 +191,37 @@ int main(int argc, char const *argv[])
     // h.add(2);
     // h.add(8);
     // h.add(1);
-    h.add(4);
-    h.add(1);
-    h.add(3);
+    
+    //2,
+    // h.add(4);
+    // h.add(1);
+    // h.add(3);
+    // h.add(2);
+    // h.add(16);
+    // h.add(9);
+    // h.add(10);
+    // h.add(14);
+    // h.add(8);
+    // h.add(7);
+
+    //3,
+    h.add(5);
+    h.add(13);
     h.add(2);
-    h.add(16);
-    h.add(9);
-    h.add(10);
-    h.add(14);
-    h.add(8);
+    h.add(25);
     h.add(7);
+    h.add(17);
+    h.add(20);
+    h.add(8);
+    h.add(4);
     h.print();
     h.build_max_heap();
     h.print();
-    // h.max_heapify_loop(2);
-    // h.print();
-    // h.min_heapify(1);
-    // h.print();
+    h.heap_extract_max();
+    h.print();
+    h.max_heap_insert(26);
+    h.print();
+    h.heap_sort();
+    h.print();
     return 0;
 }
