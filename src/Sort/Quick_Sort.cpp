7cacc814b37edced3c1c76c8c5fa24417e1fb3f5
Sean <SeanLynn@users.noreply.github.com>
2016-04-29 22:05:13 +0800

    Update Quick_Sort.cpp

--- src/Sort/Quick_Sort.cpp
+++ src/Sort/Quick_Sort.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <random>
+#include <ctime>
 using namespace std;
 
 void print(int *A, int p, int r){
@@ -23,6 +25,7 @@ int partition(int *A, int p, int r){
     return pivot;
 }
 
+
 void quick_sort(int *A, int p, int r){
     // sort A[p...r]
     if(p < r){
@@ -32,11 +35,49 @@ void quick_sort(int *A, int p, int r){
     }
 }
 
+int RANDOM(int l, int r){
+    //random_device rd;
+    //int ran = l + rd() % (r - l + 1);
+    //cout << ran << endl;
+    
+    srand((int)time(0));
+    return l+rand()%(r-l+1);
+}
+
+int randomized_partition(int* A, int p, int r){
+    int i = RANDOM(p, r);
+    swap(A[r], A[i]);
+    return partition(A, p, r);
+}
+
+void randomized_quick_sort(int *A, int p, int r){
+// sort A[p...r]
+    if(p < r){
+        int pivot = randomized_partition(A, p, r);
+        randomized_quick_sort(A, p, pivot-1);
+        randomized_quick_sort(A, pivot+1, r);
+    }
+}
+
+int v[100000];
+
+
 int main(int argc, char const *argv[])
 {
-    int v[10] = {4,5,0,2,4,9,4,4,2,4};
-    print(v, 0, 9);
-    quick_sort(v, 0, 9);
-    print(v, 0, 9);
+    double dur;
+    clock_t start,end;
+    start = clock();
+    random_device rd;
+    for(int n = 0; n < 100000; ++n){
+        v[n] = rd()%1000000;
+    }
+    //print(v, 0, 999);
+    quick_sort(v, 0, 99999);
+    //randomized_quick_sort(v, 0, 9999999);
+    end = clock();
+    dur = (double)(end - start);
+    printf("Use Time:%f\n",(dur/CLOCKS_PER_SEC));
+    //print(v, 0, 99999);
+    
     return 0;
 }
