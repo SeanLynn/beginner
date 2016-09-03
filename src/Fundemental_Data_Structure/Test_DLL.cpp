3b92a5e5c8013ec3814c0c6eaa264cf772198d20
SeanLynn <syk.neu@gmail.com>
2016-09-03 23:14:35 +0800

    add DoubleLinkedList structure. two file: *.h Test_*.cpp

---
+++ src/Fundemental_Data_Structure/Test_DLL.cpp
@@ -0,0 +1,21 @@
+#include "DoubleLinkedList.h"
+
+int main(int argc, char const *argv[])
+{
+    DoubleLinkedList<int> dll;
+    for (int i = 0; i < 20; i++) {
+        dll.insert(i);
+    }
+    dll.printAll();
+    dll._delete(19);
+    dll._delete(18);
+    dll._delete(0);
+    dll.printAll();
+    dll._delete(22);
+    Node<int>* temp = dll.search(22);
+    if (temp)
+        cout << temp->getKey() << endl;
+    else
+        cout << "The Linked List doesn't have 22." << endl;
+    return 0;
+}
