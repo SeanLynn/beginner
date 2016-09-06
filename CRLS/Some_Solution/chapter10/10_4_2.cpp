17c5342641d8c9bfab33b0d25abe384462f3e978
SeanLynn <syk.neu@gmail.com>
2016-09-06 20:55:44 +0800

    add 10_4_3.cpp and 10_4_5.cpp

---
+++ CRLS/Some_Solution/chapter10/10_4_2.cpp
@@ -0,0 +1,17 @@
+#include "../../Fundemental_Data_Structure/Tree/BinaryTree.h"
+
+int main(int argc, char const *argv[])
+{
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(213);
+    tree.insert(21);
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(121);
+    tree.insert(9);
+    tree.preOrder();
+    tree.inOrder();
+    tree.postOrder();
+    return 0;
+}
---
+++ CRLS/Some_Solution/chapter10/10_4_5.cpp
@@ -0,0 +1,15 @@
+#include "../../Fundemental_Data_Structure/Tree/BinaryTree.h"
+
+int main(int argc, char const *argv[])
+{
+    BinaryTree<int> tree;
+    tree.insert(10);
+    tree.insert(213);
+    tree.insert(21);
+    tree.insert(1);
+    tree.insert(2);
+    tree.insert(121);
+    tree.insert(9);
+    tree.traversalNonRecursion();
+    return 0;
+}
